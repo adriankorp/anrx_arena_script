9ebf731efd5417fceb60f102cb945ef75de08ad2
adriankorp <adrian.korpowski@gmail.com>
2022-07-15 16:55:47 +0200

    add authToken.json and cards.js

---
+++ cards.js
@@ -0,0 +1,65 @@
+export const cards = [
+    {
+        "cardNumber": 1,
+        "nftId": 1111,
+        "powerLevel": 5,
+        "imageUrl": "https://anrxbackend.s3.us-west-2.amazonaws.com/oracleShards/5.jpg",
+        "NFTName": "OS",
+        "tokenAddress": "0x7da61ac624ea81c27dfda88950f4610423bd242c",
+        "boosted": false
+    },
+    {
+        "cardNumber": 2,
+        "nftId": 1111,
+        "powerLevel": 5,
+        "imageUrl": "https://anrxbackend.s3.us-west-2.amazonaws.com/oracleShards/5.jpg",
+        "NFTName": "OS",
+        "tokenAddress": "0x7da61ac624ea81c27dfda88950f4610423bd242c",
+        "boosted": false
+    },
+    {
+        "cardNumber": 3,
+        "nftId": 1111,
+        "powerLevel": 4,
+        "imageUrl": "https://anrxbackend.s3.us-west-2.amazonaws.com/oracleShards/4.jpg",
+        "NFTName": "OS",
+        "tokenAddress": "0x7da61ac624ea81c27dfda88950f4610423bd242c",
+        "boosted": false
+    },
+    {
+        "cardNumber": 4,
+        "nftId": 111,
+        "powerLevel": 3,
+        "imageUrl": "https://anrxbackend.s3.us-west-2.amazonaws.com/pvpbattle/battle_pass.jpg",
+        "NFTName": "BP",
+        "tokenAddress": "0xa0dc38c7b07a02c1e4d4aa0d8530304e8dd4ba4f",
+        "boosted": false
+    },
+    {
+        "cardNumber": 5,
+        "nftId": 1111,
+        "powerLevel": 2,
+        "imageUrl": "https://anrxbackend.s3.us-west-2.amazonaws.com/oracleShards/2.jpg",
+        "NFTName": "OS",
+        "tokenAddress": "0x7da61ac624ea81c27dfda88950f4610423bd242c",
+        "boosted": false
+    },
+    {
+        "cardNumber": 6,
+        "nftId": 11111,
+        "powerLevel": 1,
+        "imageUrl": "https://anrxbackend.s3.us-west-2.amazonaws.com/oracleShards/1.jpg",
+        "NFTName": "OS",
+        "tokenAddress": "0x7da61ac624ea81c27dfda88950f4610423bd242c",
+        "boosted": false
+    },
+    {
+        "cardNumber": 7,
+        "nftId": 11111,
+        "powerLevel": 0,
+        "imageUrl": "https://anrxbackend.s3.us-west-2.amazonaws.com/oracleShards/0.jpg",
+        "NFTName": "OS",
+        "tokenAddress": "0x7da61ac624ea81c27dfda88950f4610423bd242c",
+        "boosted": false
+    }
+]
